e20ba036b54b6bf28d92e485307a1d550fefa3fe
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-30 14:49:10 -0700

    added more js w/o api functionality

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,38 @@
 let weather = {
+    apiKey: "3d55e81da180b4b9506f13e0fcd1161b"
+
+
+displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather;
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
     
-}
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Dallas");
